5ddf2b596807a478c46dfe3472891065ea6c56de
eugehm <61255729+eugehm@users.noreply.github.com>
2026-05-14 23:01:36 +0000

    Deploying to gh-pages from @ eugehm/portfolio@5de320dc80c5a5d1a72dc93de1a3c4b91bbe06fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -133,51 +133,26 @@ ninja.data = [{
             window.location.href = "/portfolio/blog/2015/formatting-and-links/";
           
         },
-      },{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+      },{id: "projects-blight-speed",
+          title: 'Blight Speed',
+          description: "Michigame Jam",
           section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/portfolio/projects/blight-speed/";
+            },},{id: "projects-defenders-of-the-dune",
+          title: 'Defenders of the Dune',
+          description: "WolverineSoft Studio",
           section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/portfolio/projects/defenders-of-the-dune/";
+            },},{id: "projects-escape-the-midwest",
+          title: 'Escape The Midwest',
+          description: "Michigame Jam",
           section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/portfolio/projects/escape-the-midwest/";
+            },},{id: "projects-example-project",
+          title: 'example project',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/portfolio/projects/9_project/";
+              window.location.href = "/portfolio/projects/example-project/";
             },},{
         id: 'social-cv',
         title: 'CV',
